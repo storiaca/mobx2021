c5699631d47dbd11a7813019958f5c5bcb990881
Aleksandar Ristic <storiaca@gmail.com>
2021-03-04 17:03:26 +0100

    Create MobX store

--- src/store.tsx
+++ src/store.tsx
@@ -2,4 +2,21 @@ import { makeAutoObservable } from "mobx";
 import sha256 from "crypto-js/sha256";
 import { createContext, useContext, useEffect, FC } from "react";
 
-export {};
+interface IBLock {
+  hash: string;
+  transactions: Array<string>;
+}
+
+class BlockchainStore {
+  blocks: Array<IBLock> = [];
+  transactions: Array<string> = [];
+
+  constructor() {
+    makeAutoObservable(this)
+  }
+
+  addTransaction(message: string) {
+    this.transactions.push(message);
+  }
+}
+export {BlockchainStore};
