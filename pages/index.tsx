191dec1b8de6896da8e659e9c5ac46366ef21f17
Aleksandar Ristic <storiaca@gmail.com>
2021-03-05 11:23:31 +0100

    Display Transactions (Observer)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,31 @@
 import { useState, FC } from "react";
 import { observer } from "mobx-react-lite";
-// import { useStore } from "src/store";
+import { useStore } from "src/store";
 
 const Home: FC = () => {
-  return <main>Home</main>;
+  return (
+    <main>
+      <Transactions />
+      <h4>Hello</h4>
+    </main>
+  );
 };
+
+const Transactions: FC = observer(() => {
+  const store = useStore();
+
+  return store.transactions.length > 0 ? (
+    <div>
+      <h2>Pending Transactions</h2>
+      <ul className="pending">
+        {store.transactions.map((transaction, index) => (
+          <li key={index}>{transaction}</li>
+        ))}
+      </ul>
+    </div>
+  ) : (
+    <div>No Transactions</div>
+  );
+});
 
 export default Home;
